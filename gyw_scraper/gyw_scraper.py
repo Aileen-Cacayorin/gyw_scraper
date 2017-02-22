26f3e181d9e3daa2958d42292122ff93e46859c8
Aileen Cacayorin <aileen.cacayorin@gmail.com>
2017-02-22 14:44:47 -0800

    scrapes table rows, need to parse and organize td by tr

--- gyw_scraper/gyw_scraper.py
+++ gyw_scraper/gyw_scraper.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask, request, redirect, render_template
-from lxml import html
+from lxml import html, etree
 import requests
+
 
 app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file , gyw_scraper.py
@@ -21,7 +22,12 @@ def home():
 def results():
     page = requests.get('https://grabyourwallet.org/Boycott%20These%20Companies.html')
     tree = html.fromstring(page.content)
-    print(tree)
-    title = tree.xpath('//a[@class="navbar-brand"]/text()')
-    print(title)
-    return render_template('results.html', title=title)
+    # table_data = tree.xpath('//tr/td/text()')
+    table_data = tree.xpath('//tr')
+    # for row in tree.xpath('//tbody/tr'):
+        # print (row.xpath('//td/text()'))
+        # row_data = (row.xpath('//td/text()'))
+        # print(row_data[3])
+
+
+    return render_template('results.html', table_data=table_data)
