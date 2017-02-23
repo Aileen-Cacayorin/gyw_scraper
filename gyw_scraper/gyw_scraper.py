19aac0f2ab495aa977f54bcc0c9aaeb275e12003
Aileen Cacayorin <aileen.cacayorin@gmail.com>
2017-02-23 14:44:17 -0800

    removes info in parentheses from brands, retrieves input from form

--- gyw_scraper/gyw_scraper.py
+++ gyw_scraper/gyw_scraper.py
@@ -4,6 +4,7 @@ from lxml import html, etree
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
 
 
 
@@ -21,48 +22,26 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/results')
+@app.route('/results', methods=['GET', 'POST'])
 def results():
+    # retrieves form input
+    brand_name = request.form.get('inputBrand')
+
+    # sends request to
     page = requests.get('https://grabyourwallet.org/Boycott%20These%20Companies.html')
     data = page.text
-    import lxml
     soup = BeautifulSoup(data, "lxml")
     table = soup.find_all('table')
     rows = table[0].find_all('tr')
-    print(len(rows))
+    brands =[]
     for row in rows:
         if len(row)>33:
             info = row.find_all('td')
             if len(info)>0:
                 brand = info[0].get_text()
                 if brand != "":
-                    print(brand)
+                    brand = re.sub(r"\(.*\)","", brand)
+                    brands.append(brand)
         else:
             continue
-    # stuff = rows[4].find_all('td')
-    # print(len(stuff))
-    # print(stuff[0].get_text())
-    # for row in rows:
-    #     cols = row.find_all('td')
-    #     print(cols)
-
-
-    # tree = html.fromstring(page.content)
-    # print(tree)
-    # table = etree.HTML(tree)
-    # print(table)
-    # table_data = tree.xpath('//tr/td/text()')
-    # table_data = tree.xpath('//tr')[0]
-    # print(table_data.xpath('//td/text()'))
-    # for row in table_data:
-    #      print (row.xpath('//td/text()'))
-    # for element in table_data:
-    #     row = html.fromstring(element)
-    #     print(row)
-    # for row in tree.xpath('//tbody/tr'):
-        # print (row.xpath('//td/text()'))
-        # row_data = (row.xpath('//td/text()'))
-        # print(row_data[3])
-
-
-    return render_template('results.html')
+    return render_template('results.html', brands=brands)
