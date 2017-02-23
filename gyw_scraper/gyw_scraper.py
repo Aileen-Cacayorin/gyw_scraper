cf13889791eda6d25b6cc8ec9afce8853e26090a
Aileen Cacayorin <aileen.cacayorin@gmail.com>
2017-02-22 23:42:18 -0800

    use beautifulsoup to scrape table with ugly if statements. need to refine

--- gyw_scraper/gyw_scraper.py
+++ gyw_scraper/gyw_scraper.py
@@ -2,6 +2,9 @@ import os
 from flask import Flask, request, redirect, render_template
 from lxml import html, etree
 import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
 
 
 app = Flask(__name__) # create the application instance :)
@@ -21,13 +24,45 @@ def home():
 @app.route('/results')
 def results():
     page = requests.get('https://grabyourwallet.org/Boycott%20These%20Companies.html')
-    tree = html.fromstring(page.content)
+    data = page.text
+    import lxml
+    soup = BeautifulSoup(data, "lxml")
+    table = soup.find_all('table')
+    rows = table[0].find_all('tr')
+    print(len(rows))
+    for row in rows:
+        if len(row)>33:
+            info = row.find_all('td')
+            if len(info)>0:
+                brand = info[0].get_text()
+                if brand != "":
+                    print(brand)
+        else:
+            continue
+    # stuff = rows[4].find_all('td')
+    # print(len(stuff))
+    # print(stuff[0].get_text())
+    # for row in rows:
+    #     cols = row.find_all('td')
+    #     print(cols)
+
+
+    # tree = html.fromstring(page.content)
+    # print(tree)
+    # table = etree.HTML(tree)
+    # print(table)
     # table_data = tree.xpath('//tr/td/text()')
-    table_data = tree.xpath('//tr')
+    # table_data = tree.xpath('//tr')[0]
+    # print(table_data.xpath('//td/text()'))
+    # for row in table_data:
+    #      print (row.xpath('//td/text()'))
+    # for element in table_data:
+    #     row = html.fromstring(element)
+    #     print(row)
     # for row in tree.xpath('//tbody/tr'):
         # print (row.xpath('//td/text()'))
         # row_data = (row.xpath('//td/text()'))
         # print(row_data[3])
 
 
-    return render_template('results.html', table_data=table_data)
+    return render_template('results.html')
