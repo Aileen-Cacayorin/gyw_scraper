53b78ff7d7be1d7ed5840a2d57ba8a710e978092
Aileen Cacayorin <aileen.cacayorin@gmail.com>
2017-02-23 19:34:14 -0800

    move scrape logic to results page. plan is to use js and jquery to search later on. on results views add logic to recreate brands list from string

--- gyw_scraper/gyw_scraper.py
+++ gyw_scraper/gyw_scraper.py
@@ -19,56 +19,41 @@ app.config.from_envvar('GYW_SCRAPER_SETTINGS', silent=True)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    page = requests.get('https://grabyourwallet.org/Boycott%20These%20Companies.html')  # sends request to Grab Your Wallet Site and parses content
+    data = page.text
+    soup = BeautifulSoup(data, "lxml")
+    table = soup.find_all('table') # finds table and rows
+    rows = table[0].find_all('tr') # this returns to many rows, need to find way to only find rows with content
+    brands = []
+    for row in rows:
+        if len(row) > 33: # limits which rows are searched
+            info = row.find_all('td') #gets td for row
+            if len(info) > 0:  # limits to rows that only returned td
+                brand = info[0].get_text().lower()  # gets first td in row, where all the brands on page are listed
+                if brand != "": # eliminates blank td
+                    brand = re.sub(r"\(.*\)", "", brand)    # removes extra info in parentheses after brand name
+                    brand = re.sub(r'[^\w\s]', '', brand) # removes punctuation from brand
+                    brands.append(brand)
+        else:
+            continue
+    return render_template('home.html', brands=brands)
 
 
 @app.route('/results', methods=['GET', 'POST'])
 def results():
-    # retrieves form input
-    brand_name = request.form.get('inputBrand').lower()
-
-    #creates regex based on brand_name
-    brand_regex = r"\b(?=\w)" + re.escape(brand_name) + r"\b(?!\w)"
-
-    # sends request to Grab Your Wallet Site and parses content
-    page = requests.get('https://grabyourwallet.org/Boycott%20These%20Companies.html')
-    data = page.text
-    soup = BeautifulSoup(data, "lxml")
-
-    #finds table and rows
-    table = soup.find_all('table')
-
-    #this returns to many rows, need to find way to only find rows with content
-    rows = table[0].find_all('tr')
-    brands =[]
-    for row in rows:
-        #limits which rows are searched
-        if len(row)>33:
-            info = row.find_all('td')
-            #only applies to rows that returned td
-            if len(info)>0:
-                #gets first td in row, where all the brands on page are listed
-                brand = info[0].get_text().lower()
-                #eliminates blank td
-                if brand != "":
-                    #removes extra info in parentheses after brand name
-                    brand = re.sub(r"\(.*\)","", brand)
-                    #removes punctuation from brand
-                    brand = re.sub(r'[^\w\s]', '', brand)
-                    brands.append(brand)
+    brand_name = request.form.get('inputBrand').lower()  # retrieves form input as string unfortunately. how to pass as list?
+    brands = request.form.get('brands').replace('[', '').replace(']', '').replace(',',"").split("' '")  #removes extra brackets, and commas, splits back into list
+    print(brands)
+    for brand in brands:
+        brand = brand.strip()
+    message=""
+    for brand in brands:
+        if re.sub(r'[^\w\s]', '', brand_name) == brand:
+            message = brand_name + " is on the list"
+            break
         else:
-            continue
-
-        message=""
-        for brand in brands:
-        # searches for brand_name in brands based on brand_regex, not specific enough, ie, will match lord with lord and taylor, better regex rule?
-            # if re.search(brand_regex, brand):
-            #     message = brand_name + " is on the list"
-            #     break
-        # matches string specifically?
-            if re.sub(r'[^\w\s]', '', brand_name) == brand:
-                message = brand_name + " is on the list"
-                break
-            else:
-                message = "no match"
+            message = "no match"
     return render_template('results.html', brands=brands, brand_name=brand_name, message=message)
+
+  #creates regex based on brand_name:
+  # brand_regex = r"\b(?=\w)" + re.escape(brand_name) + r"\b(?!\w)"
