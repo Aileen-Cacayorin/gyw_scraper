1e62ea6408b671baec302dfca4e34ded4c636687
Aileen Cacayorin <aileen.cacayorin@gmail.com>
2017-03-01 21:13:01 -0800

    get jquery/ajax working

--- gyw_scraper/gyw_scraper.py
+++ gyw_scraper/gyw_scraper.py
@@ -19,30 +19,30 @@ app.config.from_envvar('GYW_SCRAPER_SETTINGS', silent=True)
 
 @app.route('/')
 def home():
-    page = requests.get('https://grabyourwallet.org/Boycott%20These%20Companies.html')  # sends request to Grab Your Wallet Site and parses content
-    data = page.text
-    soup = BeautifulSoup(data, "lxml")
-    table = soup.find_all('table') # finds table and rows
-    rows = table[0].find_all('tr') # this returns to many rows, need to find way to only find rows with content
+    return render_template('home.html')
+
+
+@app.route('/_results')
+def results():
+    brand_name = request.args.get('brand_name').lower()
+    page = requests.get(
+        'https://grabyourwallet.org/Boycott%20These%20Companies.html')  # sends request to Grab Your Wallet Site and parses content
+    stuff = page.text
+    soup = BeautifulSoup(stuff, "lxml")
+    table = soup.find_all('table')  # finds table and rows
+    rows = table[0].find_all('tr')  # this returns to many rows, need to find way to only find rows with content
     brands = []
     for row in rows:
-        if len(row) > 33: # limits which rows are searched
-            info = row.find_all('td') #gets td for row
+        if len(row) > 33:  # limits which rows are searched
+            info = row.find_all('td')  # gets td for row
             if len(info) > 0:  # limits to rows that only returned td
                 brand = info[0].get_text().lower()  # gets first td in row, where all the brands on page are listed
-                if brand != "": # eliminates blank td
-                    brand = re.sub(r"\(.*\)", "", brand)    # removes extra info in parentheses after brand name
-                    brand = re.sub(r'[^\w\s]', '', brand) # removes punctuation from brand
+                if brand != "":  # eliminates blank td
+                    brand = re.sub(r"\(.*\)", "", brand)  # removes extra info in parentheses after brand name
+                    brand = re.sub(r'[^\w\s]', '', brand)  # removes punctuation from brand
                     brands.append(brand)
         else:
             continue
-    return render_template('home.html', brands=brands)
-
-
-@app.route('/results', methods=['GET','POST'])
-def results():
-    brand_name = request.form.get('inputBrand').lower()
-    brands = json.loads(request.form.get('brands'))
     message=""
     for brand in brands:
         if re.sub(r'[^\w\s]', '', brand_name) == brand:
@@ -50,7 +50,8 @@ def results():
             break
         else:
             message = "no match"
-    return render_template('results.html', brands=brands, brand_name=brand_name, message=message)
+    # return render_template('results.html', brands=brands, brand_name=brand_name, message=message)
+    return jsonify(message=message)
 
 
   #creates regex based on brand_name:
